55f3be306ddadd3173695bfdfce9bd4a84e36339
arisu6804 <arisu6804@gmail.com>
2026-06-12 17:16:47 +0530

    feat: add NotFound page

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+import EmptyState from '../components/EmptyState.jsx';
+
+/**
+ * Catch-all 404 page for unknown routes.
+ */
+export default function NotFound() {
+  return (
+    <EmptyState
+      icon="🧭"
+      title="Page not found"
+      message="The page you are looking for does not exist."
+      action={
+        <Link to="/" className="btn btn-primary">
+          Go home
+        </Link>
+      }
+    />
+  );
+}
